9d722f10fd71c545f6b5a185403c1313a67939df
Daniel Biondo <dan.m.biondo@gmail.com>
2020-02-16 12:35:00 -0500

    bare bones of the program structure

---
+++ FoodInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FoodInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FoodInfoTest/TestClasses/MenuTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FoodInfoTest
+{
+    [TestClass]
+    public class MenuTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
